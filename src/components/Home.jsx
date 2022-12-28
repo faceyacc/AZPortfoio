f1bca9b8c0f626af3100653bd821fe4d802f2b90
Ty <justfacey@gmail.com>
2022-12-28 09:29:31 -0500

    init commit

---
+++ src/components/Home.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+import Social from "./Social";
+
+const Home = () => {
+  return (
+    <>
+      <div className="tokyo_tm_home">
+        <div className="home_content">
+          <div className="avatar">
+            <div
+              className="image avatar_img"
+              style={{
+                backgroundImage: "url(assets/img/slider/BIA1.gif)",
+              }}
+            >
+            </div>
+            {/* END AVATAR IMAGE */}
+          </div>
+          {/* END AVATAR */}
+          <div className="details">
+            <h3 className="name">A to Z Studio</h3>
+            <p className="job">
+            Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, 
+            totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi
+            architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia volup
+            </p>
+            {/* END JOB */}
+            <Social />
+          </div>
+          {/* END DETAILS */}
+        </div>
+        {/* END HOME CONTENT */}
+      </div>
+      {/* END HOME */}
+    </>
+  );
+};
+
+export default Home;
